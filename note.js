a5ae65ae12719ff2b678f38c92dd2e5f432ebc6f
ibalaji777 <balajiofficialmailme@gmail.com>
2024-09-08 12:19:23 +0530

    notes

---
+++ note.js
@@ -0,0 +1,20 @@
+// npm install path-browserify --save-dev
+
+
+// npx webpack
+
+// const path = require('path');
+
+// module.exports = {
+//     entry: './index.js',  // Adjust the path to your entry file
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     mode: 'development',
+//     resolve: {
+//         fallback: {
+//             "path": require.resolve("path-browserify")
+//         }
+//     }
+// };
